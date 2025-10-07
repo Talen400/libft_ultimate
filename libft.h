401411d9dde77beee340a66972412d09678ffce4
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-10-07 16:51:56 -0300

    ajust Make and header to ft_strcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tlavared <tlavared@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 15:02:50 by tlavared          #+#    #+#             */
-/*   Updated: 2025/09/09 00:41:43 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/10/07 16:47:39 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
